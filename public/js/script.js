918fe911f7355753fb598f8419bf4a67c5a47919
R4dche <radchenko.g97@gmail.com>
2022-09-13 23:27:44 +0300

    final

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,22 @@
 let myTitleSwiper = new Swiper('.title-swiper',{
     slidesPerView: 1,
     // spaceBetween: 30,
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true
+    },
+    mousewheel: {
+        sensitivity: 1,
+        // eventsTarget: ".slide-title"
+    },
+    
 });
 let myTextSwiper = new Swiper('.text-swiper',{
     slidesPerView: 1,
+    mousewheel: {
+        sensitivity: 1,
+        // eventsTarget: ".slide-title"
+    },
 });
 myTitleSwiper.controller.control = myTextSwiper;
 myTextSwiper.controller.control = myTitleSwiper;
